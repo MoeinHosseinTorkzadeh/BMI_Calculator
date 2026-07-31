dccb7b8b4fb659ca818629115c750459ade0d968
Moein Hossein Torkzadeh <moeintorkzadeh2005@gmail.com>
2026-07-31 20:35:49 +0400

    edited history_page from one single card to multiple card using ListView.builder and bmi_model we built before

--- lib/screens/history_page.dart
+++ lib/screens/history_page.dart
@@ -1,6 +1,7 @@
 import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/components/bmi_history_card_details.dart';
+import '../database/bmi_model.dart';
 
 class HistoryPage extends StatefulWidget {
   @override
@@ -22,46 +23,42 @@ class _HistoryPageState extends State<HistoryPage> {
 }
 
 class BmiHistoryCard extends StatelessWidget {
-  const BmiHistoryCard({super.key});
+  //Instead of a normal list we use BmiRecord model we built to make the work much more simple
+  final List<BmiRecord> bmiRecords = [
+    BmiRecord(
+      bmi: 25.5,
+      category: 'Normal Weight',
+      height: 168.2,
+      weight: 71.2,
+      age: 21,
+      gender: 'Male',
+      date: '31 July 2026',
+    ),
+    BmiRecord(
+      bmi: 22.4,
+      category: 'Normal Weight',
+      height: 180.0,
+      weight: 72.2,
+      age: 21,
+      gender: 'Male',
+      date: '30 July 2026',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Card(
-        color: kInactiveCardColor,
-        child: Column(
-          spacing: 5,
-          children: [
-            HistoryPageCardDetails(
-              bmi: 23.1,
-              category: 'Normal Weight',
-              height: 178,
-              weight: 75,
-              age: 29,
-              gender: 'Male',
-              date: '26 May 2025',
-            ),
-            HistoryPageCardDetails(
-              bmi: 25,
-              category: 'Normal Height',
-              height: 180,
-              weight: 72,
-              age: 21,
-              gender: 'Female',
-              date: '26 July 2025',
-            ),
-            HistoryPageCardDetails(
-              bmi: 25,
-              category: 'Normal Height',
-              height: 180,
-              weight: 72,
-              age: 21,
-              gender: 'Female',
-              date: '31 June 2025',
-            )
-          ],
-        ),
-      ),
+    return ListView.builder(
+      itemCount: bmiRecords.length,
+      itemBuilder: (context, index) {
+        return HistoryPageCardDetails(
+            bmi: bmiRecords[index].bmi,
+            category: bmiRecords[index].category,
+            height: bmiRecords[index].height,
+            weight: bmiRecords[index].weight,
+            age: bmiRecords[index].age,
+            date: bmiRecords[index].date,
+            gender: bmiRecords[index].gender);
+      },
     );
   }
 }
